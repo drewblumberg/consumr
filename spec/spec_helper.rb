b810a16a77034dd94789e343fc95e7d0cda05b97
Drew Blumberg <alblumberg21@gmail.com>
2014-03-11 16:05:52 -0500

    Added nav styling

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ require 'capybara/rspec'
 require 'webmock/rspec'
 
 # Disable external connections
-WebMock.disable_net_connect!(allow_localhost: true)
+# WebMock.disable_net_connect!(allow_localhost: true)
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
